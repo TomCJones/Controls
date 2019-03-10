5a883b66623fecd7c711e55b011f5e01e2e5f8ca
tom jones <thomasclinganjones@gmail.com>
2019-03-09 16:13:12 -0800

    add areas/contacts/pages

--- Controls/Areas/Contacts/Pages/AddContact.cshtml.cs
+++ Controls/Areas/Contacts/Pages/AddContact.cshtml.cs
@@ -5,22 +5,45 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Controls.Data;
 using Controls.Models;
+using Microsoft.AspNetCore.Identity;
 
-namespace Controls.Areas.Contacts
+namespace Controls.Areas.Contacts.Pages
 {
     public class AddContactModel : PageModel
     {
+
+        private readonly UserManager<UserObject> _userManager;
+        private readonly SignInManager<UserObject> _signInManager;
+        private readonly IEmailSender _emailSender;
         private readonly Controls.Data.ControlsDbContext _context;
 
-        public AddContactModel(Controls.Data.ControlsDbContext context)
+        public AddContactModel(UserManager<UserObject> userManager,
+                        SignInManager<UserObject> signInManager,
+                        IEmailSender emailSender,
+                        Controls.Data.ControlsDbContext context)
         {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _emailSender = emailSender;
             _context = context;
         }
+        public string Username { get; set; }
 
-        public IActionResult OnGet()
+        public async Task<IActionResult> OnGetAsync()
         {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            var userName = await _userManager.GetUserNameAsync(user);
+            var email = await _userManager.GetEmailAsync(user);
+            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+
+            Username = userName;
+
             return Page();
         }
 
---
+++ Controls/Areas/Contacts/Pages/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Controls.Data;
+using Controls.Models;
+
+namespace Controls.Areas.Contacts.Pages
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Controls.Data.ControlsDbContext _context;
+
+        public DetailsModel(Controls.Data.ControlsDbContext context)
+        {
+            _context = context;
+        }
+
+        public ContactLink ContactLink { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ContactLink = await _context.contactLinks.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (ContactLink == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
