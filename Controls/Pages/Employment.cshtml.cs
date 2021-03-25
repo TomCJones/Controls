485c56bd8090f4d6999adcc121ca3b95dbacde8f
tom jones <thomasclinganjones@gmail.com>
2021-03-25 15:42:15 -0700

    ensure call updates from 2019-12 are saved

---
+++ Controls/Pages/Employment.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Controls.Models;
+
+namespace Controls.Pages
+{
+    public class EmploymentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
